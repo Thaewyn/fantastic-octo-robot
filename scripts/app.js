d07c0baad91b69087d0baeaccffd2a1d6c5e1ec8
Thaewyn <thaewyn@gmail.com>
2019-03-18 17:30:29 -0400

    adding weapon equipping function

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,13 @@
 //some random app file
+var player;
 
 var appController = {
     init: function() {
         console.log("initialize the app!");
+        player = {
+            hp: 100,
+            weapon: "stick"
+        }
     },
     randomNumber: function(maximum, isInteger) {
         var rand = Math.random() * maximum;
@@ -11,7 +16,18 @@ var appController = {
         } else {
             return rand;
         }
+    },
+    weaponList: ["sword", "axe", "bow", "dagger"],
+    equipWeapon: function(whichWeapon, playerObj) {
+        if (playerObj) {
+            playerObj.weapon = this.weaponList[whichWeapon];
+        } else {
+            this.player.weapon = this.weaponList[whichWeapon];
+        }
+        return true;
     }
 }
 
 appController.init();
+
+appController.equipWeapon(2);
