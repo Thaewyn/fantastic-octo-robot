20bceaa0f3703e5289c4ca363659b63f92507532
Thaewyn <thaewyn@gmail.com>
2019-03-18 17:36:42 -0400

    Made a function for adding monsters

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,12 @@
 //some random app file
 
+var monsterList = ["goblin", "warg", "ogre", "troll"];
+var monster;
+
 var appController = {
     init: function() {
         console.log("initialize the app!");
+        monster = this.makeMonster();
     },
     randomNumber: function(maximum, isInteger) {
         var rand = Math.random() * maximum;
@@ -11,7 +15,21 @@ var appController = {
         } else {
             return rand;
         }
+    },
+    makeMonster: function(whichMonster) {
+        var newMonster = {
+            name: "",
+            hp: 5
+        }
+        if (whichMonster) {
+            newMonster.name = monsterList[whichMonster];
+        } else {
+            newMonster.name = monsterList[this.randomNumber(monsterList.length, true)];
+        }
+        return newMonster;
     }
 }
 
 appController.init();
+
+console.log("We have a monster! It's a "+monster.name);
